c3c2e31458f507ed2d5536e1b6a277e3b663ed7b
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2023-10-30 19:14:43 +0600

    initial project and initial type

---
+++ src/constants/constants.ts
@@ -0,0 +1,27 @@
+import meatImage from '../assets/meat.png';
+import cheeseImage from '../assets/cheese.png';
+import cabbageImage from '../assets/cabbage.png';
+import beaconImage from '../assets/bacon.png';
+
+const INGREDIENTS = [
+  {
+    name: 'meat',
+    price: 80,
+    image: meatImage
+  },
+  {
+    name: 'cheese',
+    price: 50,
+    image: cheeseImage
+  },
+  {
+    name: 'salad',
+    price: 10,
+    image: cabbageImage
+  },
+  {
+    name: 'beacon',
+    price: 50,
+    image: beaconImage
+  },
+];
